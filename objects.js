5762d93e1e8bd9752473c85aabec2f9fb76ce263
Keith Gordon <keithg07@gmail.com>
2017-02-08 04:51:29 +0000

    halfway completed

--- objects.js
+++ objects.js
@@ -1,10 +1,15 @@
-playlist = {
-  Kanye West: 'Ultralight Beam',
-  Jay z: 'New York',
+var playlist = {
+  Kanye: 'Ultralight Beam',
+  Jay: 'New York',
   Drake: 'Both'
-}
+};
 
 function updatePlaylist(obj,key,value ) {
-  Object.assign({},obj , {key : [value]});
+  obj[key] = value;
+  return obj;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 }
