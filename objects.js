b22634d5d794f5efb85d5d3a398d50e6d590d552
Tibor Schöll <scti1016@hs-karlsruhe.de>
2017-03-23 01:20:37 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = { 'bla' : '123'};
+
+function updatePlaylist( obj, artist, song){
+
+  obj[artist] = song;
+
+  return obj;
+
+}
+
+ function removeFromPlaylist (obj, artist){
+  delete obj[artist];
+
+  return obj;
+  
+}
