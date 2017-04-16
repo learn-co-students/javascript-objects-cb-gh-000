659ff0c5a774b9f18656f0d4f48b1e46d0c93de3
Oscar Caceres <caceres.oscar7@gmail.com>
2017-04-16 09:33:29 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  lenon:'imagine'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+   playlist[artistName]=songTitle;
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+}
