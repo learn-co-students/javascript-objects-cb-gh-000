5fe16550c0845fe826423742f36465bda6102aa8
shiva reddy <readytohackk@gmail.com>
2017-05-17 19:21:14 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist={
+  jsj:"hi"
+};
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist[artistName]="hiihi"
+playlist[songTitle]="joj"};
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName];
+  playlist;
+};
