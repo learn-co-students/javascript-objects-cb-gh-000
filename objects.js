971a78e8b1f806ca182c54a863d450c2de67eda0
Jerald Mark Buljatin <bjeraldmark@gmail.com>
2017-06-02 06:26:41 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = { "Ed Sheeran": "castle on the hill"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
+
+  return playlist
+}
+
+updatePlaylist('Phil Ochs',"Here's to the State of Mississippi")
