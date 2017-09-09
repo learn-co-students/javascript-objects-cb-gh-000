549f358775c200622abba1c316796bbaf5dd6dea
Daniel Constantine <danielkevinconstantine@gmail.com>
2017-09-09 14:11:49 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,9 +1,14 @@
-var playlist = {};
+var playlist = {
+  Naruto: "The legend"
+};
 
 function updatePlaylist(playlist, artist, song){
-
+  playlist[artist] = song;
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
 
+  return playlist
 }
