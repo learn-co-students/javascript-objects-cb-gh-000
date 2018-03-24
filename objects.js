d32ae041355fdf963ff3eac3b5639cd583ca41d3
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-24 17:17:36 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,11 +1,14 @@
 var playlist = {
- MyRiot: 'Sen',
-};
+  'Marine Boudeau': 'Something',
+  'Someone else': 'Something else'
+}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  return delete playlist.artistName;
+  delete playlist[artistName]
+  return playlist
 }
