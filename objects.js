34c31bf35f3ea2e7dd9a0239b309d51d67136f20
VISHAL RAJ <sunilkroy1973@gmail.com>
2018-06-15 06:37:35 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+
+var playlist = { enrique : 'bailando'  }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+  playlist.artistName = songTitle
+  
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  delete playlist.artistName
+}
+
