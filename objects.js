69a895650f9d2499feb73f5fd0a46ba7455cc139
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-15 02:54:09 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {Sting: 'Englishman in Newyork'}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
