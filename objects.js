e7ae7e42d5b13021b9a67db09dbd9b90f69d73ca
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-28 14:31:22 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {firstSong: "Smoorverliefd"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist
+}
